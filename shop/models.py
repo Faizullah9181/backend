c752097749698dacda42805e8ecd03f957c4bb60
FAIZULLAH <faizullah9181@gmail.com>
2023-02-11 14:54:03 +0530

    .

--- shop/models.py
+++ shop/models.py
@@ -20,6 +20,8 @@ class Product(models.Model):
     countInStock = models.IntegerField(null=True, blank=True, default=0)
     createdAt = models.DateTimeField(auto_now_add=True)
     _id = models.AutoField(primary_key=True, editable=False)
+    category = models.ForeignKey(
+        'Category', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
@@ -91,3 +93,11 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return str(self.address)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
